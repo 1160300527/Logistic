e260288ba5df460bdb4d4bed6cc7382798adad8f
1160300527 <15683609851@163.com>
2018-10-26 00:33:05 +0800

    finish

--- Logistic.py
+++ Logistic.py
@@ -15,12 +15,12 @@ test_num0 = 200  # 测试集中Y=0类大小
 test_num1 = 200
 step = 0.01  # 梯度下降步长
 max_iter = 20000  # 梯度下降最大迭代次数
-stop_value = 0.00000001  # 梯度下降退出条件
-L = math.e**(-8)    #正则项系数
+stop_value = 0.000001  # 梯度下降退出条件
+L = math.e**(-8)  # 正则项系数
 
 
 # 当X维度为2时，用于画出分类图像
-def draw(X0,X1,Y, W,choose):  # 画函数图像
+def draw(X0, X1, Y, W, choose):  # 画函数图像
     X = np.vstack((X0, X1))
     plt.title("Logistic function for test"+choose+"\nAccuracy="+str("%.2f" % accuracy(X, Y, W))+"    Precision=" +
               str("%.2f" % precision(X, Y, W))+"    Recall="+str("%.2f" % recall(X, Y, W)))
@@ -40,10 +40,10 @@ def newton(X, Y):
     iter = 0
     while((iter < 100)):
         try:
-            H_I = Hessian(X,W).I
+            H_I = Hessian(X, W).I
         except:
             return W
-        change = H_I.dot(gra(X,Y,W).T).T
+        change = H_I.dot(gra(X, Y, W).T).T
         W = W + change
         if (change.dot(change.T) < stop_value):
             break
@@ -57,10 +57,10 @@ def newton_reg(X, Y):
     iter = 0
     while((iter < 100)):
         try:
-            H_I = Hessian(X,W).I
+            H_I = Hessian(X, W).I
         except:
             return W
-        change = H_I.dot((gra(X,Y,W)-L*W).T).T
+        change = H_I.dot((gra(X, Y, W)-L*W).T).T
         W = W + change
         if (change.dot(change.T) < stop_value):
             break
@@ -68,22 +68,23 @@ def newton_reg(X, Y):
     return W
 
 
-def Newton(X,Y,X0_Test,X1_Test,Y_Test):
-    W = newton(X,Y)
-    W_reg = newton(X,Y)
+# 牛顿法计算W并画出图像
+def Newton(X, Y, X0_Test, X1_Test, Y_Test):
+    W = newton(X, Y)
+    W_reg = newton(X, Y)
     plt.subplot(121)
-    draw(X0_Test,X1_Test,Y_Test,W,"by Newton method without regular")
+    draw(X0_Test, X1_Test, Y_Test, W, "by Newton method without regular")
     plt.subplot(122)
-    draw(X0_Test,X1_Test,Y_Test,W_reg,"by Neton method with regular")
+    draw(X0_Test, X1_Test, Y_Test, W_reg, "by Neton method with regular")
     plt.show()
 
 
-#求Hessian矩阵  X:n*(m+1)   W:1*(m+1)
-def Hessian(X,W):
-    temp = possibility(X,W) #1*n
-    sum = np.multiply(temp,(1-temp))    #1*n
-    A = np.multiply(sum,np.eye(X.shape[0]))
-    return X.T.dot(A).dot(X)#(m+1)*(m+1)
+# 求Hessian矩阵  X:n*(m+1)   W:1*(m+1)
+def Hessian(X, W):
+    temp = possibility(X, W)  # 1*n
+    sum = np.multiply(temp, (1-temp))  # 1*n
+    A = np.multiply(sum, np.eye(X.shape[0]))
+    return X.T.dot(A).dot(X)  # (m+1)*(m+1)
 
 
 # 梯度下降法
@@ -112,16 +113,6 @@ def gradient_reg(X, Y):
     return W
 
 
-def Gradient(X,Y,X0_Test,X1_Test,Y_Test):
-    W = gradient(X,Y)
-    W_reg = gradient_reg(X,Y)
-    plt.subplot(121)
-    draw(X0_Test,X1_Test,Y_Test,W,"by gradient without regular")
-    plt.subplot(122)
-    draw(X0_Test,X1_Test,Y_Test,W_reg,"by gradient with regular")
-    plt.show()
-
-
 # 求梯度大小
 def gra(X, Y, W):
     P = Y-possibility(X, W)  # 1*num
@@ -136,6 +127,17 @@ def possibility(X, W):
     temp = np.exp(sum)
     p = 1-1 / (1+temp)
     return p
+
+
+# 梯度上升方法计算W值并画出图像
+def Gradient(X, Y, X0_Test, X1_Test, Y_Test):
+    W = gradient(X, Y)
+    W_reg = gradient_reg(X, Y)
+    plt.subplot(121)
+    draw(X0_Test, X1_Test, Y_Test, W, "by gradient without regular")
+    plt.subplot(122)
+    draw(X0_Test, X1_Test, Y_Test, W_reg, "by gradient with regular")
+    plt.show()
 
 
 # 计算准确度
@@ -163,6 +165,7 @@ def precision(X, Y, W):
     return TP/all_one
 
 
+# 计算recall值
 def recall(X, Y, W):
     real_one = 0
     TP = 0
@@ -184,26 +187,40 @@ def birthX(lable, loc, scale, num):
     return X.transpose()
 
 
+# 随机生成数据并进行测试
 def logistic():
     X0 = np.mat(birthX(0, loc0, scale0, num0))
     X1 = np.mat(birthX(1, loc1, scale1, num1))
     X0_Test = np.mat(birthX(0, loc0, scale0, test_num0))
     X1_Test = np.mat(birthX(1, loc1, scale1, test_num1))
-    Y_Test = np.mat(np.hstack((np.linspace(0, 0, test_num0), np.linspace(1, 1, test_num1))))
+    Y_Test = np.mat(
+        np.hstack((np.linspace(0, 0, test_num0), np.linspace(1, 1, test_num1))))
     X = np.mat(np.vstack((X0, X1)))
     Y = np.mat(np.hstack((np.linspace(0, 0, num0), np.linspace(1, 1, num1))))
-    #W = gradient_reg(X, Y, m)
-    Gradient(X,Y,X0_Test,X1_Test,Y_Test)
-    Newton(X,Y,X0_Test,X1_Test,Y_Test)
+    Gradient(X, Y, X0_Test, X1_Test, Y_Test)
+    Newton(X, Y, X0_Test, X1_Test, Y_Test)
 
 
+# 读取数据集文件并完成训练及测试过程
 def trainFromuci():
-    (X,Y) = read_train()
+    # 从训练集文件获取训练样本
+    (X, Y) = read_train()
+    # 计算通过不同优化方法得到的W
     W = gradient(X, Y)
-    W_reg = gradient_reg(X,Y)
-    W_Newton = newton(X,Y)
-    W_Newton_Reg = newton_reg(X,Y)
-    (X_test,Y_test) = read_test(W)
+    W_reg = gradient_reg(X, Y)
+    W_Newton = newton(X, Y)
+    W_Newton_Reg = newton_reg(X, Y)
+    # 从测试集文件获取测试样本
+    (X_test, Y_test) = read_test()
+    print("The data from UCI:00267")
+    print("By Gradient Ascent Without Regular: ")
+    print_data(X_test, Y_test, W)
+    print("By Gradient Ascent With Regular:")
+    print_data(X_test, Y_test, W_reg)
+    print("By Newton Wethod Without Regular:")
+    print_data(X_test, Y_test, W_Newton)
+    print("By Newton Method With Regular:")
+    print_data(X_test, Y_test, W_Newton_Reg)
 
 
 def read(filename):
@@ -213,12 +230,14 @@ def read(filename):
     finally:
         f.close()
 
-#读取训练
+# 读取训练数据文件，得到训练数据集
+
+
 def read_train():
     Y = []
-    content = read('train.txt')
-    N = len(content)
-    M = len(content[0].split(','))-1
+    content = read('train.txt')  # 打开训练数据文件
+    N = len(content)  # 数据集文件中每个样本属性值以\n分割
+    M = len(content[0].split(','))-1  # 数据集文件以,分割不同数据
     X = np.linspace(0, 0, M+1)
     for line in content:
         value = line.rstrip('\n').split(',')
@@ -230,17 +249,13 @@ def read_train():
     X = np.delete(X, 0, axis=0)
     X = np.mat(X)
     Y = np.mat(Y)
-    W = gradient(X, Y)
-    W_reg = gradient_reg(X,Y)
-    W_Newton = newton(X,Y)
-    W_Newton_Reg = newton_reg(X,Y)
-    return X,Y
+    return X, Y
 
 
-#读取测试文件并进行通过学习的W进行计算并得到accuracy,precision,recall
-def read_test(W):
+# 读取测试数据文件，得到测试数据集
+def read_test():
     Y = []
-    content = read('test.txt')
+    content = read('test.txt')  # 打开测试数据文件
     M = len(content[0].split(','))-1
     X = np.linspace(0, 0, M+1)
     for line in content:
@@ -253,11 +268,17 @@ def read_test(W):
     X = np.delete(X, 0, axis=0)
     X = np.mat(X)
     Y = np.mat(Y)
-    return X,Y
-    print("accuracy:"+str("%.2f" % accuracy(X, Y, W)))
-    print("precision:"+str("%.2f" % precision(X, Y, W)))
-    print("recall:"+str("%.2f" % recall(X, Y, W)))
+    return X, Y
 
 
+# 打印输出Accuracy,precision,recall
+def print_data(X, Y, W):
+    print("accuracy:"+str("%.2f" % accuracy(X, Y, W)))  # 计算accuracy并输出
+    print("precision:"+str("%.2f" % precision(X, Y, W)))  # 计算precision并输出
+    print("recall:"+str("%.2f" % recall(X, Y, W)))  # 计算recall并输出
+
+
+# 随机生成数据并进行训练及测试
 logistic()
+# 通过从UCI获取的数据进行训练及测试
 trainFromuci()
